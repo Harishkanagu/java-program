278dc3d929019b3474ea6b6921c3472ca5a067bd
Harish kanagu <harimsd4455@gmail.com>
2025-07-01 12:40:39 +0530

    Addition

---
+++ add.java
@@ -0,0 +1,12 @@
+public class add {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a=20;
+        b=30;
+        c=a+b;
+        System.out.println(c);
+        }
+    
+}
