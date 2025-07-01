af38b5f51157a77206416e137e631e231c3822e3
Harish kanagu <harimsd4455@gmail.com>
2025-07-01 19:24:07 +0530

    Student details

---
+++ studentdetails.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class studentdetails 
+{
+    int regnum;
+    String Name;
+    long phone;
+    Scanner scanner=new Scanner(System.in);
+    System.out.print("Enter the Register number:");
+    regnum=scanner.nextInt();
+    System.out.print("Enter the Name:");
+    Name=scanner.nextString();
+    System.out.print("Enter the Phone number:");
+    phone=scanner.nextLong();
+    scanner.close();
+    
+    public void display()
+    {
+        System.out.print("Student details");
+        System.out.print("Register num:"+regnum);
+        System.out.print("Student Name:"+ Name)
+        System.out.print("Phone number:"+phone);
+    }
+public static void main(String[] args)
+{
+    studentdetails s=new studentdetails();
+    s.display();
+
+}
+}
