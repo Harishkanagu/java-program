f20447987a45cb75808e8bb4e75077c022002e79
Harish kanagu <harimsd4455@gmail.com>
2025-07-03 19:56:33 +0530

    package of clac

---
+++ Calc/clac.java
@@ -0,0 +1,16 @@
+package Calc;
+
+public class clac {
+    public  void pow(int a,int b)
+    {
+    double result = Math.pow(a,b);
+    System.out.println("Power:"+result);
+    }
+    public void mod(int a,int b)
+    {
+        double result=a%b;
+        System.out.println("Modulus"+result);
+        
+    }
+
+}
