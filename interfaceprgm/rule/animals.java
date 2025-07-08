389974f97a8e3243eebca8628ec767cab5a6fa3a
Harish kanagu <harimsd4455@gmail.com>
2025-07-08 18:19:34 +0530

    Animals program

--- interfaceprgm/rule/animals.java
+++ interfaceprgm/rule/animals.java
@@ -1,4 +1,17 @@
 package interfaceprgm.rule;
 
-public class animals {
+public class animals implements rules{
+    public void protection(){
+        System.out.println("Protection for Animals:\n"+"Maintain food chain," +
+                "Ecosystem balance,\n"+"Need to maintain biodiversity..\n");
+
+    }
+    public void importance(){
+        System.out.println("Importance of need of Animals\n");
+        System.out.println("Animals are similar to Human....\n" +
+                "they need Air,Water,sunlight and food based on the animals..");
+
+
+
+    }
 }
