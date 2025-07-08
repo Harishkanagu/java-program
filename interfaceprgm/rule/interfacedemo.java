c7a8dec4eea1b5511b2ff94b861ee162f6069840
Harish kanagu <harimsd4455@gmail.com>
2025-07-08 18:20:41 +0530

    Interface Main program

--- interfaceprgm/rule/interfacedemo.java
+++ interfaceprgm/rule/interfacedemo.java
@@ -1,4 +1,13 @@
 package interfaceprgm.rule;
 
 public class interfacedemo {
+    public static void main (String args[]){
+        rules Rules =new plant();
+        Rules.protection();
+        Rules.importance();
+        rules Rules_1 =new animals();
+        Rules_1.protection();
+        Rules_1.importance();
+
+    }
 }
