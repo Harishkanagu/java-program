ed9083ef42b0625af67b509bb99f061f6f62bf47
Harish <harimsd4455@gmail.com>
2025-07-10 18:48:34 +0530

    Main class of calc

---
+++ Calc/demo.java
@@ -0,0 +1,17 @@
+import Calc.*;
+class demo{
+    public static void main(String[] args) {
+        clac c=new clac();
+        adaclac a=new adaclac();
+        a.add(5, 7);       
+        a.sub(10, 3);       
+        a.mul(2, 4);
+        a.div(20, 5);
+
+
+        c.pow(3,4);
+        c.mod(2,4);
+
+
+    }
+}
