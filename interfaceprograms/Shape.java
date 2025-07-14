517250bca46415bae2e6f5d6116c4a0873a3e241
Harish <harimsd4455@gmail.com>
2025-07-14 18:54:13 +0530

    Abstractsclassprogram

---
+++ interfaceprograms/Shape.java
@@ -0,0 +1,12 @@
+package abstracts;
+
+import java.util.Scanner;
+
+public abstract class Shape{
+    public abstract void Areaofshape();
+
+    public void Display() {
+        System.out.println("Thank You!....");
+
+    }
+}
