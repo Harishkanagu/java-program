1acf5fc5dfa39a92844560b54650b2a194e6eded
Harish <harimsd4455@gmail.com>
2025-08-07 18:54:26 +0530

    Employes details using map

---
+++ Map_class.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Workerdetails{
+    String address;
+    int salary;
+    int emply_id;
+
+    public Workerdetails(String address,int salary,int emply_id){
+        this.address=address;
+        this.salary=salary;
+        this.emply_id=emply_id;;
+
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public String getAddress(){
+        return address;
+    }
+
+    public int getEmplyid(){
+        return emply_id;
+    }
+
+}
+public class Map_class {
+    public static void main(String args[]) {
+        Map<String,ArrayList<Workerdetails>> map = new HashMap<>();
+
+        Workerdetails wdname1=new Workerdetails("Coimbatore",90000,19);
+        Workerdetails wdname2=new Workerdetails("Tiruppur",80000,13);
+        Workerdetails wdname3=new Workerdetails("Erode",70000,12);
+        ArrayList<Workerdetails> dtsworker_1=new ArrayList<>();
+        dtsworker_1.add(wdname1);
+        ArrayList<Workerdetails> dtsworker_2=new ArrayList<>();
+        dtsworker_2.add(wdname2);
+        ArrayList<Workerdetails> dtsworker_3=new ArrayList<>();
+        dtsworker_3.add(wdname3);
+        map.put("Hari",dtsworker_1);
+        map.put("Mythili",dtsworker_2);
+        map.put("Praveen",dtsworker_3);
+         for(Workerdetails workers:dtsworker_1){
+             System.out.println("IDnum:"+workers.getEmplyid());
+             System.out.println("Address:"+workers.getAddress());
+             System.out.println("Salary:"+workers.getSalary());
+         }
+
+    }
+}
